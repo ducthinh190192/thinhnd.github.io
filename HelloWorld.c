823ee74cdd1f803228201fe7d07d2e26caa8198f
ducthinh190192 <nguyenducthinhbkhn@gmail.com>
2022-06-04 23:23:13 +0700

    Add files via upload

---
+++ HelloWorld.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+ printf ("Hello world!\n");
+ printf ("Nguyễn Hồng Phi!\n");
+ int a = 4;
+ int b = 5; 
+printf ("a + b = %d\n", a+b);
+printf ("a - b = %d\n", a-b);
+printf ("a * b = %d\n", a*b);
+system("pause");
+ return 0;
+ 
+}
